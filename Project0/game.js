8edb8b51f747a0a53e552c84de6a6fdc82361a79
tawbin nawshin <tawbin.nawshin@gmail.com>
2019-03-13 01:00:11 -0400

    Project0 All Files

--- Project0/game.js
+++ Project0/game.js
@@ -1,76 +1,74 @@
+// Arcade Shooter game
 var canvas = document.getElementById('canvas');
 // Get the canvas drawing context
 var context = canvas.getContext('2d');
 
-// Create an object representing a square on the canvas
+//call the makesquare function to draw the box
+var shooter = makeSquare(50, canvas.height / 2 - 25, 50, 5);
+var bullet = makeSquare(0, 0, 10, 10);
+var up = false;
+var down = false;
+var space = false;
+var shooting = false;
+var enemies = [];
+var enemyBaseSpeed = 2; 
+var score = 0;
+var timeBetweenEnemies = 5 * 1000;//default 5 secs
+var timeoutId = null;
+
+//x coordinate , y coordinate , length, speed 
 function makeSquare(x, y, length, speed) {
-  return {
+  //make the square object for either shooter or bullet
+  var square =  
+  {
     x: x,
     y: y,
     l: length,
     s: speed,
-    draw: function() {
+    //draw function is defined to draw this box in the canvas
+    draw: function () {
       context.fillRect(this.x, this.y, this.l, this.l);
     }
   };
+  return square;
 }
 
-// The ship the user controls
-var ship = makeSquare(50, canvas.height / 2 - 25, 50, 5);
-
-// Flags to tracked which keys are pressed
-var up = false;
-var down = false;
-var space = false;
-
-// Is a bullet already on the canvas?
-var shooting = false;
-// The bulled shot from the ship
-var bullet = makeSquare(0, 0, 10, 10);
-
-// An array for enemies (in case there are more than one)
-var enemies = [];
-
-// Add an enemy object to the array
-var enemyBaseSpeed = 2;
 function makeEnemy() {
   var enemyX = canvas.width;
-  var enemySize = Math.round((Math.random() * 15)) + 15;
-  var enemyY = Math.round(Math.random() * (canvas.height - enemySize * 2)) + enemySize;
+  //give the enemy boxes different size and different speed 
+  var enemySize = Math.round((Math.random() * 15)) + 20;
+  var enemyY = Math.round(Math.random() * (canvas.height - enemySize * 3)) + enemySize;
   var enemySpeed = Math.round(Math.random() * enemyBaseSpeed) + enemyBaseSpeed;
+  //adding the square to the enemies' array 
   enemies.push(makeSquare(enemyX, enemyY, enemySize, enemySpeed));
 }
 
-// Check if number a is in the range b to c (exclusive)
-function isWithin(a, b, c) {
-  return (a > b && a < c);
+// Check if number square1 is in the range square2 and square3 (exclusive)
+function isWithin(coordinate1, coordinate2, coordinate3) {
+  return (coordinate1 > coordinate2 && coordinate1 < coordinate3);
 }
 
 // Return true if two squares a and b are colliding, false otherwise
-function isColliding(a, b) {
+function isColliding(sq1, sq2) {
   var result = false;
-  if (isWithin(a.x, b.x, b.x + b.l) || isWithin(a.x + a.l, b.x, b.x + b.l)) {
-    if (isWithin(a.y, b.y, b.y + b.l) || isWithin(a.y + a.l, b.y, b.y + b.l)) {
+  if (isWithin(sq1.x, sq2.x, sq2.x + sq2.l) || isWithin(sq1.x + sq1.l, sq2.x, sq2.x + sq2.l)) 
+  {
+    if (isWithin(sq1.y, sq2.y, sq2.y + sq2.l) || isWithin(sq1.y + sq1.l, sq2.y, sq2.y + sq2.l)) {
       result = true;
     }
   }
   return result;
 }
 
-// Track the user's score
-var score = 0;
-// The delay between enemies (in milliseconds)
-var timeBetweenEnemies = 5 * 1000;
-// ID to track the spawn timeout
-var timeoutId = null;
-
 // Show the game menu and instructions
-function menu() {
-  erase();
-  context.fillStyle = '#000000';
+function menu() 
+{
+  //Reset the canvas 
+  reset();
+  context.fillStyle = 'white';
   context.font = '36px Arial';
   context.textAlign = 'center';
-  context.fillText('Shoot \'Em!', canvas.width / 2, canvas.height / 4);
+  context.fillText('Shoot using space bar!', canvas.width / 2, canvas.height / 4);
   context.font = '24px Arial';
   context.fillText('Click to Start', canvas.width / 2, canvas.height / 2);
   context.font = '18px Arial';
@@ -81,11 +79,20 @@ function menu() {
 
 // Start the game
 function startGame() {
-	// Kick off the enemy spawn interval
+  // Kick off the enemy spawn interval(5 secs). 
   timeoutId = setInterval(makeEnemy, timeBetweenEnemies);
   // Make the first enemy
   setTimeout(makeEnemy, 1000);
   // Kick off the draw loop
+  // An array for enemies (in case there are more than one)
+  enemies = [];
+
+  // Track the user's score
+  score = 0;
+
+  // The delay between enemies (in milliseconds)
+  timeBetweenEnemies = 5 * 1000;
+
   draw();
   // Stop listening for click events
   canvas.removeEventListener('click', startGame);
@@ -93,18 +100,22 @@ function startGame() {
 
 // Show the end game screen
 function endGame() {
-	// Stop the spawn interval
+  // Stop the spawn interval
   clearInterval(timeoutId);
   // Show the final score
-  erase();
-  context.fillStyle = '#000000';
+  reset();
+  context.fillStyle = 'white';
   context.font = '24px Arial';
   context.textAlign = 'center';
   context.fillText('Game Over. Final Score: ' + score, canvas.width / 2, canvas.height / 2);
+  context.font = '24px Arial';
+  context.fillText('Click to Start', canvas.width / 2, (canvas.height / 4) * 3);
+  canvas.addEventListener('click', startGame);
 }
 
 // Listen for keydown events
-canvas.addEventListener('keydown', function(event) {
+canvas.addEventListener('keydown', function (event) {
+  //preventing the default key event
   event.preventDefault();
   if (event.keyCode === 38) { // UP
     up = true;
@@ -117,8 +128,15 @@ canvas.addEventListener('keydown', function(event) {
   }
 });
 
+// Shoot the bullet (if not already on screen)
+function shoot() {
+    shooting = true;
+    bullet.x = shooter.x + shooter.l;
+    bullet.y = shooter.y + shooter.l / 2;
+  }
+
 // Listen for keyup events
-canvas.addEventListener('keyup', function(event) {
+canvas.addEventListener('keyup', function (event) {
   event.preventDefault();
   if (event.keyCode === 38) { // UP 
     up = false;
@@ -129,98 +147,95 @@ canvas.addEventListener('keyup', function(event) {
 });
 
 // Clear the canvas
-function erase() {
-  context.fillStyle = '#FFFFFF';
-  context.fillRect(0, 0, 600, 400);
-}
-
-// Shoot the bullet (if not already on screen)
-function shoot() {
-  if (!shooting) {
-    shooting = true;
-    bullet.x = ship.x + ship.l;
-    bullet.y = ship.y + ship.l / 2;
-  }
+function reset() {
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, 1000, 600);
 }
 
 // The main draw loop
-function draw() {
-  erase();
+function draw() 
+{
+  reset();
   var gameOver = false;
+  
   // Move and draw the enemies
-  enemies.forEach(function(enemy) {
-    enemy.x -= enemy.s;
-    if (enemy.x < 0) {
+  enemies.forEach(function (enemy) {
+    enemy.x = enemy.x-enemy.s; 
+    if (enemy.x < 0) 
+    {
       gameOver = true;
     }
-    context.fillStyle = '#00FF00';
+    context.fillStyle = '#20EDB6';
     enemy.draw();
   });
-  // Collide the ship with enemies
-  enemies.forEach(function(enemy, i) {
-    if (isColliding(enemy, ship)) {
+
+
+  // Collide the shooter with enemies
+  enemies.forEach(function (enemy)
+   {
+    if (isColliding(enemy, shooter)) {
       gameOver = true;
     }
   });
-  // Move the ship
-  if (down) {
-    ship.y += ship.s;
+  // Move the shooter up and down
+  if (down) 
+  {
+    shooter.y += shooter.s;  //incrementing the y coordinate with the speed factor
   }
-  if (up) {
-    ship.y -= ship.s;
+  if (up) 
+  {
+    shooter.y -= shooter.s;   //decrementing the y coordinate with the speed factor
   }
-  // Don't go out of bounds
-  if (ship.y < 0) {
-    ship.y = 0;
+  // check that it doesn't go out of bounds
+  if (shooter.y < 0) 
+  {
+    shooter.y = 0;
   }
-  if (ship.y > canvas.height - ship.l) {
-    ship.y = canvas.height - ship.l;
+
+  if (shooter.y > canvas.height - shooter.l) 
+  {
+    shooter.y = canvas.height - shooter.l;
   }
-  // Draw the ship
-  context.fillStyle = '#FF0000';
-  ship.draw();
+
+// Draw the shooter
+context.fillStyle = '#C70039';
+shooter.draw();
+
   // Move and draw the bullet
-  if (shooting) {
+  if (shooting) 
+  {
     // Move the bullet
     bullet.x += bullet.s;
     // Collide the bullet with enemies
-    enemies.forEach(function(enemy, i) {
+    enemies.forEach(function (enemy, index) {
       if (isColliding(bullet, enemy)) {
-        enemies.splice(i, 1);
+        enemies.splice(index, 1); //splice will remove
         score++;
         shooting = false;
-        // Make the game harder
-        if (score % 10 === 0 && timeBetweenEnemies > 1000) {
-          clearInterval(timeoutId);
-          timeBetweenEnemies -= 1000;
-          timeoutId = setInterval(makeEnemy, timeBetweenEnemies);
-        } else if (score % 5 === 0) {
-          enemyBaseSpeed += 1;
-        }
       }
     });
-    // Collide with the wall
-    if (bullet.x > canvas.width) {
-      shooting = false;
-    }
+    
     // Draw the bullet
-    context.fillStyle = '#0000FF';
+    context.fillStyle = '#9820ED';
     bullet.draw();
   }
+
   // Draw the score
-  context.fillStyle = '#000000';
+  context.fillStyle = 'white';
   context.font = '24px Arial';
   context.textAlign = 'left';
   context.fillText('Score: ' + score, 1, 25)
   // End or continue the game
-  if (gameOver) {
+  if (gameOver) 
+  {
     endGame();
-    
-  } else {
-    window.requestAnimationFrame(draw);
+  } else 
+  {
+    window.requestAnimationFrame(draw);  //to animate another frame at the next repaint
   }
 }
 
 // Start the game
 menu();
+// keep the focus on a canvas element always, no matter where I click on the page
 canvas.focus();
--- Project0/project0.js
+++ Project0/project0.js
@@ -1,83 +1,101 @@
 //We will register the body load event to call GetValuesfromStorage to polulate all input field values 
-window.onload = function() {
-   let form = document.getElementById('formSignup');
-   form.addEventListener('load',GetValuesfromStorage());
+window.onload = function () {
+    let form = document.getElementById('formSignup');
+    if(form)
+    form.addEventListener('load', GetValuesfromStorage());
 }
-   
-function Validate()
-{
-    let firstName = document.getElementById('txtfirstName');
-    let lastName = document.getElementById('txtlastName');
-    let middleName = document.getElementById('txtmiddleName');
 
-    if(firstName.value == middleName.value)
-    {
+function Validate() {
+    let firstName = document.getElementById('txtfirstName').value;
+    let lastName = document.getElementById('txtlastName').value;
+    let middleName = document.getElementById('txtmiddleName').value;
+    var country = document.getElementById("txtcountry").value;
+
+//validate the properties
+    if (firstName == middleName) {
         alert("firstname and middlename cannot be the same");
         return false;
     }
-    if(middleName.value == lastName.value)
-    {
+    else if (middleName == lastName) {
         alert("middlename and lastname cannot be the same");
         return false;
     }
 
-    if(firstName.value == lastName.value)
-    {
+    else if (firstName == lastName) {
         alert("firstname and lastname cannot be the same");
         return false;
-    }
-    return ValidateCountry();
-}
-
-function StoreValues(e)
-{
-        localStorage.setItem(e.id,e.value);
-}
-
-function storeGender(e)
-{
-    localStorage.setItem('genderkey', e.value); 
-}
-
-function GetValuesfromStorage()
-{  
-    document.frmSignup.gender.value = localStorage.genderkey;  
-
-    let keys = Object.keys(localStorage); //Gets all keys in local Storage
-    let values = Object.values(localStorage); //Gets all Values in local Storage
-    if(keys!=undefined && keys.length>0)
-    {
-       for(var i= 0; i<keys.length;i++)
-       {
-           if(document.getElementById(keys[i])!=null && document.getElementById(keys[i])!=undefined)
-           {
-              document.getElementById(keys[i]).value = values[i];
-           }
-       }
-    }
-}
-function ValidateCountry(){
-    var country = document.getElementById("txtcountry");
-    if(country != 'USA' || country !='US' || country != 'United States' || country != 'United States Of America')
-    {
+    }else if (!(country == 'USA' || country == 'US' || country == 'United States' || country == 'United States Of America') ){
         alert("User has to reside in the United States to Sign Up");
         return false;
     }
-    else return true;
+    else
+    {
+        alert("Thank you for signing up with Revature!");
+        return true;
+    }
+
 }
 
-function signIn(){
-    var id = document.getElementById("txtusername").value;
-    var password = document.getElementById("txtpsw").value;
-    if((id =='Admin' || id =='admin') && (password == 123))
+function StoreValues(e) {
+    localStorage.setItem(e.id, e.value);
+}
+
+function storeGender(e) {
+    localStorage.setItem('genderkey', e.value);
+}
+
+function GetValuesfromStorage() {
+    document.frmSignup.gender.value = localStorage.genderkey;
+
+    let keys = Object.keys(localStorage); //Gets all keys in local Storage
+    let values = Object.values(localStorage); //Gets all Values in local Storage
+    if (keys != undefined && keys.length > 0) {
+        for (var i = 0; i < keys.length; i++) {
+            if (document.getElementById(keys[i]) != null && document.getElementById(keys[i]) != undefined) {
+                document.getElementById(keys[i]).value = values[i];
+            }
+        }
+    }
+}
+function ValidateCountry() {
+    
+
+}
+function validateContact()
+{
+    let firstName = document.getElementById('txtfName');
+    let lastName = document.getElementById('txtlName');
+    let txtemail = document.getElementById('txtemail');
+    let txtquery = document.getElementById('query');
+
+    if (firstName.value == lastName.value) {
+        alert("firstname and lastname cannot be the same");
+        return false;
+    }
+    else if(txtemail.value == "" || txtquery.value=="")
     {
-        window.location.href = "game_Project0.html"
+        alert("Email and Query cannot be empty");
+        return false;
     }
     else 
     {
+        alert("Thank you for submitting your query. We will contact you within next 24 hours.");
+        window.location.href = "project0.html"
+    }
+    return true;
+}
+
+function signIn() {
+    var id = document.getElementById("txtusername").value;
+    var password = document.getElementById("txtpsw").value;
+    if ((id == 'Admin' || id == 'admin') && (password == 123)) {
+        alert("Successful Sign In! Taking you to the game page");
+        window.location.href = "game_Project0.html"
+    }
+    else {
         alert("Username and password is not valid. Please enter a correct username and password");
         return false;
-        
+
     }
 }
 
